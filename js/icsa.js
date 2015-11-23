299650cd00f1b79a3cb51db030d64c7ade9d80ee
asaybel <pyjioh@gmail.com>
2015-11-23 19:24:17 +0500

    Add correct masonry resizing, remove unnecessary tags

--- js/icsa.js
+++ js/icsa.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
-    $('#tokenfield').tokenfield();
-});
+(function(root, $, undefined) {
+    'use strict';
+
+    $(function () {
+        $('#tokenfield').tokenfield();
+
+        $('#tokenfield')
+            .on('tokenfield:createtoken', function (e) {
+                alert('Token has been added! Token value is: ' + e.attrs.value)
+            })
+            .on('tokenfield:edittoken', function (e) {
+                //alert('Token has been edited! Token value is: ' + e.attrs.value)
+            })
+            .on('tokenfield:removedtoken', function (e) {
+                alert('Token has been removed! Token value was: ' + e.attrs.value)
+            })
+            .tokenfield()
+
+    });
+
+} (this, jQuery));
