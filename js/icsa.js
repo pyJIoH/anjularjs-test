63441c2edeffb623e59e7edcb4cb72f48f039cbb
asaybel <pyjioh@gmail.com>
2015-11-27 14:50:28 +0500

    Sorting doesn't work with masonry

--- js/icsa.js
+++ js/icsa.js
@@ -1,36 +1,6 @@
 (function(root, $, undefined) {
     'use strict';
 
-    jQuery.fn.sortElements = (function () {
-        var sort = [].sort;
-        return function (comparator, getSortable) {
-            getSortable = getSortable || function () {
-                    return this;
-                };
-            var placements = this.map(function () {
-                var sortElement = getSortable.call(this),
-                    parentNode = sortElement.parentNode,
-                    nextSibling = parentNode.insertBefore(
-                        document.createTextNode(''),
-                        sortElement.nextSibling
-                    );
-                return function () {
-
-                    if (parentNode === this) {
-                        throw new Error(
-                            "You can't sort elements if any one is a descendant of another."
-                        );
-                    }
-                    parentNode.insertBefore(this, nextSibling);
-                    parentNode.removeChild(nextSibling);
-                };
-            });
-            return sort.call(this, comparator).each(function (i) {
-                placements[i].call(getSortable.call(this));
-            });
-        };
-    })();
-
     $(function () {
         $('#tokenfield').tokenfield();
 
@@ -61,14 +31,17 @@
         }.bind(this));
 
         $('#btn-load-more').on('click', function(data){
-            $('.grid-item').sortElements(function(a, b){
-                return $(a).data('relevant') < $(b).data('relevant') ? 1 : -1;
+            var $divs = $("div.grid-item");
+            var orderedTabs = $divs.sort(function (a, b) {
+                return $(a).data('relevant') < $(b).data('relevant');
             });
-            //var $divs = $("div.grid-item");
-            //var orderedTabs = $divs.sort(function (a, b) {
-            //    return $(a).data('relevant') < $(b).data('relevant');
-            //});
-            //$(".grid").html(orderedTabs);
+            $(orderedTabs).each(function(index) {
+                this.style.left = '0px';
+                this.style.top = '0px';
+            });
+
+            $(".grid").html(orderedTabs);
+            //$('.grid').masonry();
         })
 
     });
